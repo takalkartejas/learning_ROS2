2373df4c146ffcd37fe7d86dfc5767939e424956
tt-213268 <takalkartejastt@gmail.com>
2022-11-12 20:19:58 +0100

    setup.py:- added the node draw_cycle

--- my_robot_controller/setup.py
+++ my_robot_controller/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "test_node = my_robot_controller.my_first_node:main"
+            "test_node = my_robot_controller.my_first_node:main",
+            "draw_cycle = my_robot_controller.draw_cycle:main"
         ],
     },
 )
