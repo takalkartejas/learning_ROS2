2aaf0d5d22987ae64f36bbf13874bf6fae9715e9
tt-213268 <takalkartejastt@gmail.com>
2022-11-12 18:42:10 +0100

    my_first_node.py - added a counter after hello to print

--- my_robot_controller/my_robot_controller/my_first_node.py
+++ my_robot_controller/my_robot_controller/my_first_node.py
@@ -10,14 +10,15 @@ class MyNode(Node): #this class inherits from Node from rclpy.node, so that it c
     def __init__(self):  #create a constructor
         super().__init__("first_node")   # the command - super calles the constructor of the upper class, we enter the nodes name inside bracketm
         #the above statement initiallizes the node
+        self.counter_ = 0
         self.get_logger().info("ROS2") # to print hello world
         self.create_timer(1.0, self.timer_callback)   # creat_timer and get_logger are the functions in the class Node in rclpy.node, 
         #we are using them here because of inheritence - self
         #the create_timer function calls a user defined callback function after every mentioned secs
 
     def timer_callback(self):
-        self.get_logger().info("hello")
-
+        self.get_logger().info("hello " + str(self.counter_))
+        self.counter_ += 1
 def main(args=None):  #arguments are set to none by default
     rclpy.init(args=args) #init the rclpy communication, args of init function are that we get from main
     node = MyNode()   # we create the node here
