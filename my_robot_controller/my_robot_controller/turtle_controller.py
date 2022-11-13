a8e89cd34a198ad96f5a16dc02b02a1b24590698
tt-213268 <takalkartejastt@gmail.com>
2022-11-13 13:17:22 +0100

    turtle_controller.py:- removed errors

--- my_robot_controller/my_robot_controller/turtle_controller.py
+++ my_robot_controller/my_robot_controller/turtle_controller.py
@@ -9,14 +9,15 @@ class TurtleControllerNode(Node):
     def __init__(self):
         super().__init__("turtle_controller")
         self.get_logger().info("Turtle controller has been started")
-        self.pose_subscriber = self.create_subscription(Pose,'/turtle1/Pose',self.pose_callback, 10)
+        self.pose_subscriber = self.create_subscription(Pose,'/turtle1/pose',self.pose_callback, 10)
         self.vel_publisher = self.create_publisher(Twist,'/turtle1/cmd_vel', 10)
         self.create_timer(1,self.vel_publisher_callback)
 
     def pose_callback(self, pose: Pose):
         self.get_logger().info(str(pose))
 
-    def vel_publisher_callback(self, msg: Twist):
+    def vel_publisher_callback(self):
+        msg = Twist()
         msg.linear.x = 0.1
         msg.angular.z = 0.1
         self.vel_publisher.publish(msg)
